6a80b7d9aa1de37249f62e73d17c7dd7707d5002
Jan Zimmermann <jan@ueberruhr.de>
2020-03-21 17:55:58 +0100

    refactored testing and project structure

--- src/test/java/NumScannerTest.java
+++ src/test/java/NumScannerTest.java
@@ -1,5 +1,19 @@
-class TestScanner {
-    static public void main(String args[]) {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumScannerTest extends NumScanner {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    public void TestScanner() {
         NumScanner scanner;
         scanner = new NumScanner();
         if (scanner.readInput("testdatei_arithmetik.txt")) {
